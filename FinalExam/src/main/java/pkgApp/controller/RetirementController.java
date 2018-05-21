79d06d0f360fe43b510aebc0efdbdbffe740faa0
dwars <dwars@128.4.112.139>
2018-05-21 18:28:55 -0400

    2nd update with figured out RegEx

--- FinalExam/src/main/java/pkgApp/controller/RetirementController.java
+++ FinalExam/src/main/java/pkgApp/controller/RetirementController.java
@@ -65,9 +65,15 @@ public class RetirementController implements Initializable {
 		// "\\d*?" - means any decimal number
 		// "\\d*(\\.\\d*)?" means any decimal, then optionally a period (.), then
 		// decmial
-		hmTextFieldRegEx.put(txtYearsToWork, "\\d*?");
-		hmTextFieldRegEx.put(txtAnnualReturnWorking, "\\d*(\\.\\d*)?");
+	//	hmTextFieldRegEx.put(txtYearsToWork, "\\d*?");
+		//hmTextFieldRegEx.put(txtAnnualReturnWorking, "\\d*(\\.\\d*)?");d
 
+		hmTextFieldRegEx.put(txtYearsToWork, "[0-9]| 1[0-9]| 2[0-9]| 3[0-9]| 4[0]");
+		hmTextFieldRegEx.put(txtAnnualReturnWorking, "[0-9](\\.[0-9]{1,2}){0,1}|10(\\.0{1,2}){0,1}");
+		hmTextFieldRegEx.put(txtYearsRetired, "[0-9]| 1[0-9]| 2[0-9]");
+		hmTextFieldRegEx.put(txtAnnualReturnRetired, "[0-9](\\.[0-9]{1,2}){0,1}|10(\\.0{1,2}){0,1}");
+		
+		
 		// Check out these pages (how to validate controls):
 		// https://stackoverflow.com/questions/30935279/javafx-input-validation-textfield
 		// https://stackoverflow.com/questions/40485521/javafx-textfield-validation-decimal-value?rq=1
